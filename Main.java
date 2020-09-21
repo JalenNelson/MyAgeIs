dc57c6a455bee768f8468d058f3569a99e8b880c
JalenNelson <jalen.nelson@patelhs.org>
2020-09-21 14:40:04 +0000

    I added two Systems. One of them is telling my current age and the other one is telling how old I will be in December.

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int myAge = 15;
+    int SoonAge = 16;
+    System.out.println("I am " + myAge + " years old");
+    System.out.println("I will be " + SoonAge +" years old on Dec,14,2020");
   }
 }
